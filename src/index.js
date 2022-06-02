8d2f57e3432b46b9f6fba2c3d4171cadbc31530b
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2022-06-02 18:00:25 +0300

    Add UI module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const firstParagragh = document.createElement('p');
-firstParagragh.textContent = 'Hello World';
-document.body.appendChild(firstParagragh);
+import { renderPage } from "./ui";
+
+document.addEventListener("DOMContentLoaded", renderPage)
---
+++ src/ui.js
@@ -0,0 +1,38 @@
+export { renderPage };
+import './style.css';
+import GithubLogo from './github.png';
+
+
+const renderPage = () => {
+    const mainWrapper = document.createElement('div');
+    mainWrapper.classList.add('main-wrapper');
+
+    const header = document.createElement("header");
+    const headerName = document.createElement("h1");
+    headerName.textContent = "Todo List";
+    header.appendChild(headerName);
+
+    const main = document.createElement("main");
+    const mainP = document.createElement("p");
+    mainP.textContent = "Lorem Ipsum";
+    main.appendChild(mainP);
+    
+    const footer = document.createElement("footer");
+    const footerDiv = document.createElement("div");
+    const footerLink = document.createElement("a");
+    const footerLinkImg = document.createElement("img");
+    footerLinkImg.classList.add('github-logo');
+    footerLinkImg.setAttribute("src", GithubLogo);
+    footerDiv.textContent = "Developed by miroslav-zarenkov";
+    footerLink.setAttribute("href", "https://github.com/miroslav-zarenkov");
+    footerLink.setAttribute("target", "_blank");
+    footerLink.setAttribute("rel", "noopener noreferrer");
+    footer.appendChild(footerDiv);
+    footer.appendChild(footerLink);
+    footerLink.appendChild(footerLinkImg);
+
+    document.body.appendChild(mainWrapper);
+    mainWrapper.appendChild(header);
+    mainWrapper.appendChild(main);
+    mainWrapper.appendChild(footer);
+};
